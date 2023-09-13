67712823e0f0696bff38c9d3e11230007263d089
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-09-13 19:01:02 -0300

    🐛: Remoção do espaço do nome da tela

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,12 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../pages/Login";
+import { SignUp } from "../pages/SignUp";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
 export function AuthRoutes() {
     return (
         <Navigator>
-            <Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <Screen name="Login" component={Login} options={{ headerShown: false, animation: "none" }}  />
+            <Screen name="SignUp" component={SignUp} options={{ headerShown: false, animation: "none"}} />
         </Navigator>
     )
 }

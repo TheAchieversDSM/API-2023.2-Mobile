fe0ad8e7f40f782bf8d4b3df4cb0774988b4e8a3
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-09-14 09:52:19 -0300

    ✨: integração com o endpoint de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,68 @@
 import { ButtonContainer, Container, InputsContainer, Logo, Scroll, SignUpContainer, TabsContainer } from "./styled";
 import oracleLogo from "../../assets/oracle.png"
+import React, { useState } from 'react'
 import { Button } from "../../components/button/button";
 import { useNavigation } from "@react-navigation/native";
 import Input from "../../components/input/input";
-import { ScrollView } from "react-native";
+import serviceUser from "../../service/user"
+import { ICreateUser } from "../../interfaces/user";
+import { verifyPasswordMatch } from "../../utils/utils";
+import { IUserCadastro } from "../../interfaces/functions";
 
 export function SignUp() {
 
     const navigate = useNavigation()
+
+    const [data, setData] = useState<ICreateUser>({
+        email: "",
+        name: "",
+        password: "",
+        rPassword: ""
+    })
+
+    const [errorMessage, setErrorMessage] = useState({
+        password: "",
+        email: ""
+    })
+    const [errorStatus, setErrorStatus] = useState({
+        email: false,
+        password: false,
+        rPassword: false,
+        name: false
+    })
+
+    const checkFields = (values: ICreateUser): boolean => {
+        const newErrorStatus = {
+            email: values.email === "",
+            name: values.name === "",
+            password: values.password === "",
+            rPassword: values.rPassword === "",
+        };
+        setErrorStatus(newErrorStatus);
+        return newErrorStatus.email || newErrorStatus.password || newErrorStatus.name || newErrorStatus.rPassword;
+    };
+
+
+    const handleSubmit = async (data: ICreateUser) => {
+        try {
+            const validate = verifyPasswordMatch(data)
+            if (checkFields(data)) {
+                return
+            } else {
+                setErrorMessage({ email: "", password: "" })
+                if (!validate) {
+                    return setErrorMessage({ email: "", password: "The passwords do not match." })
+                }
+                const error: IUserCadastro | undefined = await serviceUser.createUser(data)
+                setErrorMessage({ email: String(error?.erro), password: "" })
+                if (error?.validacao) {
+                    navigate.navigate("Login")
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Scroll>
@@ -33,26 +88,40 @@ export function SignUp() {
                         <Input
                             placeholder={"Name"}
                             textColor="#000"
-                            onChange={() => { }}
                             iconL="user"
+                            errorMsg={errorStatus.name ? "Name is required" : ""}
+                            onChange={(e) => setData({ ...data, name: e.nativeEvent.text })}
                         />
                         <Input
                             placeholder={"E-mail"}
                             textColor="#000"
-                            onChange={() => { }} iconL="envelope"
+                            iconL="envelope"
+                            errorMsg={errorStatus.email ?
+                                errorMessage.email ?
+                                    errorMessage.email : "Email is required"
+                                : errorMessage.email}
+                            onChange={(e) => setData({ ...data, email: e.nativeEvent.text })}
                         />
                         <Input
                             placeholder={"Password"}
                             password
                             textColor="#000"
-                            onChange={() => { }}
+                            errorMsg={errorStatus.password ?
+                                errorMessage.password ?
+                                    errorMessage.password : "Password is required"
+                                : errorMessage.password}
+                            onChange={(e) => setData({ ...data, password: e.nativeEvent.text })}
                             iconL={"lock"}
                         />
                         <Input
                             placeholder={"Repeat Password"}
                             password
                             textColor="#000"
-                            onChange={() => { }}
+                            errorMsg={errorStatus.rPassword ?
+                                errorMessage.password ?
+                                    errorMessage.password : "Password is required"
+                                : errorMessage.password}
+                            onChange={(e) => setData({ ...data, rPassword: e.nativeEvent.text })}
                             iconL={"lock"}
                         />
                     </InputsContainer>
@@ -62,7 +131,7 @@ export function SignUp() {
                             type='solid'
                             borderColor='red'
                             backgroundColor='red'
-                            onPress={() => navigate.navigate("Login")}
+                            onPress={() => handleSubmit(data)}
                         />
                     </ButtonContainer>
                 </SignUpContainer>

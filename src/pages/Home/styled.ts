24cf4be230cd845af3c227ac242557054ca56b57
Mariana Tamay <maritamay4@gmail.com>
2023-09-14 22:55:06 -0300

    ✨ API20232-55 Tela Home - Criação da tela home

---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,35 @@
+import { Text } from "@rneui/themed";
+import { SafeAreaView, View } from "react-native";
+import {styled} from "styled-components/native";
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: #393939;
+  width: 100%;
+`;
+
+export const Box = styled(SafeAreaView)`
+  padding: 20px
+`;
+
+export const ViewContainer = styled(View)`
+  flex: 1;
+  background-color: #fff;
+  width: 100%;
+  borderTopLeftRadius: 30px;
+  borderTopRightRadius: 30px;
+  marginTop: 20px;
+`;
+
+export const TextTitle = styled(Text)`
+    color: #c74634;
+    font-size: 22px;
+    fontWeight: bold;
+`;
+
+export const SubTextTitle = styled(Text)`
+    color: #c74634;
+    font-size: 16px;
+    fontWeight: bold;
+    marginTop: 10px;
+`;

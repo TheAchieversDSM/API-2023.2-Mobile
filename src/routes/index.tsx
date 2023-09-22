b8715b9df37cfcec910d12499b480c25c8f1bd74
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-09-22 19:58:37 -0300

    ✨: Vereficação do status da aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,31 +2,23 @@ import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StackRoutes } from "./stack.routes";
 import { useAuth } from "../hooks/auth";
-import { decodeJsonWebToken } from "../utils/utils";
+import { checkTokenValidity, decodeJsonWebToken } from "../utils/utils";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
     const { userToken, signOut } = useAuth();
 
     useEffect(() => {
-        /* @REVIEW - Mudar o metodo de verificação */
-        const checkTokenValidity = () => {
+        const intervalId = setInterval(() => {
             if (userToken) {
-                const { exp } = decodeJsonWebToken(userToken)
-                const expireTime = new Date(exp * 1000);
-                const timeNow = new Date();
-                if (expireTime < timeNow) {
-                    signOut()
-                }
+                checkTokenValidity(userToken, signOut);
             }
-        };
-        checkTokenValidity();
-        const intervalId = setInterval(checkTokenValidity, 30 * 1000);
+        }, 3600 * 1000);
         return () => {
             clearInterval(intervalId);
         };
-    }, [userToken]);
-
+    }, [userToken, signOut]);
+    
     return (
         <NavigationContainer>
             {userToken ? <StackRoutes /> : <AuthRoutes />}

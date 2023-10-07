91b5b2abd721f6189af089868edaae701d2f69fc
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-10-07 16:18:12 -0300

    ⚙️: Configurações do projeto

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,7 @@
 declare module "@env" {
   const URL_API: string;
   const APP_SECRET: string;
-  export {URL_API, APP_SECRET};
+  const APP_MODE: "dev" | "main";
+  const AZURE_API: string;
+  export {URL_API, APP_SECRET, APP_MODE, AZURE_API};
 }

f7d8f2c763e5d62c0b811346eae18ff57e23e619
Mariana Tamay <maritamay4@gmail.com>
2023-11-26 00:40:35 -0300

    Fix 🐛: Remoção do ícone a mais
    Co-authored-by: Matheus.Sakuragui <MatheusSakuragui@users.noreply.github.com>

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -342,7 +342,6 @@ export const Cards = (props: ICards) => {
             { color: "#000", name: "hourglass-o", size: 23, function: toggleTimerModal, icon: "FontAwesome" },
             { color: "#000", name: "files-o", size: 26, function: handleOpenFileModal, icon: "FontAwesome" },
             { color: "#000", name: "edit-2", size: 23, function: ModalEditFunction, icon: "Feather" },
-            { color: "#000", name: "files-o", size: 26, function: handleOpenFileModal, icon: "FontAwesome" }
         ]
     } else {
         options= [
@@ -352,7 +351,6 @@ export const Cards = (props: ICards) => {
             { color: "#000", name: "edit-2", size: 23, function: ModalEditFunction, icon: "Feather" },
             { color: "#000", name: "files-o", size: 26, function: handleOpenFileModal, icon: "FontAwesome" },
             { color: "#000", name: "users", size: 23, function: toggleCompModal, icon: "Feather" },
-            { color: "#000", name: "files-o", size: 26, function: handleOpenFileModal, icon: "FontAwesome" }
         ]
     }
 
